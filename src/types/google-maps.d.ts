a59a388cd160d6201729567befa975a95d523345
yassine <yassine@mbp-de-adam.home>
2025-12-14 22:48:54 +0100

    tt

---
+++ src/types/google-maps.d.ts
@@ -0,0 +1,45 @@
+/// <reference types="google.maps" />
+
+// Déclaration globale pour le namespace google
+declare global {
+  namespace google {
+    namespace maps {
+      namespace places {
+        interface PlaceResult {
+          formatted_address?: string
+          geometry?: {
+            location: {
+              lat(): number
+              lng(): number
+            }
+          }
+          [key: string]: any
+        }
+        
+        class Autocomplete {
+          constructor(input: HTMLInputElement, options?: any)
+          addListener(event: string, callback: () => void): void
+          getPlace(): PlaceResult
+        }
+      }
+      
+      class Geocoder {
+        geocode(request: any, callback: (results: any[], status: string) => void): void
+      }
+      
+      class DistanceMatrixService {
+        getDistanceMatrix(request: any, callback: (response: any, status: string) => void): void
+      }
+      
+      enum TravelMode {
+        DRIVING
+      }
+      
+      enum UnitSystem {
+        METRIC
+      }
+    }
+  }
+}
+
+export {}
